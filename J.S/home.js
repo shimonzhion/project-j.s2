aa352bf6c35755f3e081d06c713830b736ad9ea3
shimonzhion <brys94204@gmail.com>
2022-07-24 10:46:57 +0300

    COMMIT2

--- J.S/home.js
+++ J.S/home.js
@@ -3,8 +3,8 @@ function homeElements(){
   localStorage.setItem('page', 'home');
     document.querySelector("#div_container").innerHTML= `<div id ="home"><div id ="div_home"><h1 id="title_home">SMB</h1>
    
-    <img id="img_logo1" src="../IMG/logo4-removebg-preview.png"/></div>
+    <img id="img_logo1" src="IMG/logo4-removebg-preview.png"/></div>
     </div>
-    <img id="home_photos" src="../IMG/pppp.jpg" style="width:500px; "/>
+    <img id="home_photos" src="IMG/pppp.jpg" style="width:500px; "/>
     `
 }
